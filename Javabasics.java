03e4861a6c9146ac68a256925f34125df9db0753
Namratha <namratha@Namrathas-MacBook-Air.local>
2020-08-28 00:19:29 +0530

    day1

---
+++ Javabasics.java
@@ -0,0 +1,17 @@
+
+public class Javabasics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a[]= {"hello", "world"};
+		//System.out.println(a[0]);
+		//System.out.println(a[1]);
+		
+		for (int i=1; i<a.length;i--) {
+			System.out.println(a[i]);
+	
+		}
+
+	}
+
+}
